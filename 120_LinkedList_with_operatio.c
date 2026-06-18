2bb94ed6806fef99e99008fc8b374d1d303b3092
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-18 19:12:06 +0530

    08/06

--- 120_LinkedList_with_operatio.c
+++ 120_LinkedList_with_operatio.c
@@ -50,18 +50,142 @@ void display_list()
     if (head != NULL)
     {
         struct node *temp;
-        temp = head;
+        temp = head; // 1500
         printf("\nlinked list element are : \n");
         while (temp != NULL)
         {
             printf("%d ", temp->data);
-            temp = temp->next;
+            temp = temp->next; // null
         }
         printf("\n");
     }
     else
     {
         printf("\nfirst create list then display\n");
+    }
+}
+void insert_at_frist()
+{
+    if (head != NULL)
+    {
+        struct node *new = (struct node *)malloc(sizeof(struct node));
+        int num;
+        printf("enter a num : ");
+        scanf("%d", &num);
+        new->data = num;
+        new->next = head;
+        head = new;
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
+    }
+}
+void delete_at_frist()
+{
+    if (head != NULL)
+    {
+        struct node *temp;
+        temp = head;
+        head = head->next;
+        printf("\n%d element is deleted\n", temp->data);
+        free(temp);
+    }
+    else
+    {
+        printf("\nfirst create list then delete\n");
+    }
+}
+void insert_at_last()
+{
+
+    if (head != NULL)
+    {
+        struct node *new = (struct node *)malloc(sizeof(struct node));
+        int num;
+        printf("enter a num : ");
+        scanf("%d", &num);
+        new->data = num;
+        new->next = NULL;
+        struct node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new;
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
+    }
+}
+void delete_at_last()
+{
+
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            struct node *temp = head;
+            printf("\n%d is deleted from list\n", temp->data);
+            free(temp);
+            head = NULL;
+        }
+        else
+        {
+            struct node *temp = head, *prev;
+            while (temp->next != NULL)
+            {
+                prev = temp;
+                temp = temp->next;
+            }
+            prev->next = NULL;
+            printf("\n%d is deleted from list\n", temp->data);
+            free(temp);
+        }
+    }
+    else
+    {
+        printf("\nfirst create list then delete\n");
+    }
+}
+void insert_at_index()
+{
+    if (head != NULL)
+    {
+        int index;
+        printf("enter index : ");
+        scanf("%d", &index); // 0
+        if (index == 0)
+        {
+            insert_at_frist();
+        }
+        else
+        {
+            struct node *temp = head;
+            int c = 0;
+            while (temp != NULL)
+            {
+                c++;
+                temp = temp->next;
+            }
+            if (index == c)
+            {
+                insert_at_last();
+            }
+            else if (index > c || index < 0)
+            {
+                printf("\ninvalid index\n");
+            }
+            else
+            {
+                // insert at middle
+            }
+        }
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
     }
 }
 void main()
@@ -82,7 +206,6 @@ void main()
         printf("Press 10 to exit\n");
         printf("choose option : ");
         scanf("%d", &num);
-
         switch (num)
         {
         case 1:
@@ -92,19 +215,19 @@ void main()
             display_list();
             break;
         case 3:
-            // insert_at_frist();
+            insert_at_frist();
             break;
         case 4:
-            // delete_at_frist();
+            delete_at_frist();
             break;
         case 5:
-            // insert_at_last();
+            insert_at_last();
             break;
         case 6:
-            // delete_at_last();
+            delete_at_last();
             break;
         case 7:
-            // insert_at_index();
+            insert_at_index();
             break;
         case 8:
             // delete_at_index();
@@ -118,6 +241,5 @@ void main()
             printf("Entered wrong number\n");
             break;
         }
-        
     }
 }
