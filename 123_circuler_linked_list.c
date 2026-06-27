d5ad8e13f9400346fe7b05d7883424a618800935
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-06-27 19:03:26 +0530

    19/06

---
+++ 123_circuler_linked_list.c
@@ -0,0 +1,367 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+struct node *head = NULL;
+
+void create_list()
+{
+    if (head == NULL)
+    {
+        int num;
+        struct node *new, *temp;
+        printf("enter a num : ");
+        scanf("%d", &num);
+        head = (struct node *)malloc(sizeof(struct node));
+        head->data = num;
+        head->next = head;
+        temp = head;
+        while (1)
+        {
+            printf("enter 1 if you want to add more element : ");
+            scanf("%d", &num); // 1
+            if (num == 1)
+            {
+                printf("enter a num : ");
+                scanf("%d", &num); // 78
+                new = (struct node *)malloc(sizeof(struct node));
+                new->data = num;
+                new->next = head;
+                temp->next = new;
+                temp = new;
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        printf("\nlist is already created\n");
+    }
+}
+
+// void display_list()
+// {
+//     if (head != NULL)
+//     {
+//         struct node *temp;
+//         temp = head;
+//         printf("\nlinked list element are : \n");
+//         while (temp->next != head)
+//         {
+//             printf("%d ", temp->data);
+//             temp = temp->next;
+//         }
+//         printf("%d ", temp->data);
+//         printf("\n");
+//     }
+//     else
+//     {
+//         printf("\nfirst create list then display\n");
+//     }
+// }
+
+void display_list()
+{
+    if (head != NULL)
+    {
+        struct node *temp;
+        temp = head;
+        printf("\nlinked list element are : \n");
+        do
+        {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        } while (temp != head);
+
+        printf("\n");
+    }
+    else
+    {
+        printf("\nfirst create list then display\n");
+    }
+}
+void insert_at_frist()
+{
+    if (head != NULL)
+    {
+        struct node *new = (struct node *)malloc(sizeof(struct node));
+        int num;
+        printf("enter a num : ");
+        scanf("%d", &num);
+        new->data = num;
+        struct node *temp;
+        temp = head;
+        while (temp->next != head)
+        {
+            temp = temp->next;
+        }
+        temp->next = new;
+        new->next = head;
+        head = new;
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
+    }
+}
+void delete_at_frist()
+{
+    if (head != NULL)
+    {
+        struct node *temp;
+        temp = head;
+        head = head->next;
+        printf("\n%d element is deleted\n", temp->data);
+        free(temp);
+    }
+    else
+    {
+        printf("\nfirst create list then delete\n");
+    }
+}
+void insert_at_last()
+{
+
+    if (head != NULL)
+    {
+        struct node *new = (struct node *)malloc(sizeof(struct node));
+        int num;
+        printf("enter a num : ");
+        scanf("%d", &num);
+        new->data = num;
+        new->next = NULL;
+        struct node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new;
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
+    }
+}
+void delete_at_last()
+{
+
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            struct node *temp = head;
+            printf("\n%d is deleted from list\n", temp->data);
+            free(temp);
+            head = NULL;
+        }
+        else
+        {
+            struct node *temp = head, *prev;
+            while (temp->next != NULL)
+            {
+                prev = temp;
+                temp = temp->next;
+            }
+            prev->next = NULL;
+            printf("\n%d is deleted from list\n", temp->data);
+            free(temp);
+        }
+    }
+    else
+    {
+        printf("\nfirst create list then delete\n");
+    }
+}
+void insert_at_index()
+{
+    if (head != NULL)
+    {
+        int index;
+        printf("enter index : ");
+        scanf("%d", &index); // 0
+        if (index == 0)
+        {
+            insert_at_frist();
+        }
+        else
+        {
+            struct node *temp = head;
+            int c = 0;
+            while (temp != NULL)
+            {
+                c++;
+                temp = temp->next;
+            }
+            if (index == c)
+            {
+                insert_at_last();
+            }
+            else if (index > c || index < 0)
+            {
+                printf("\ninvalid index\n");
+            }
+            else
+            {
+                struct node *new = (struct node *)malloc(sizeof(struct node));
+                int num;
+                printf("enter a num : ");
+                scanf("%d", &num);
+                new->data = num;
+                new->next = NULL;
+                // index = 2
+                struct node *prev, *temp;
+                int i = 0;
+                temp = head;
+                while (i < index)
+                {
+                    i++;
+                    prev = temp;
+                    temp = temp->next;
+                }
+                prev->next = new;
+                new->next = temp;
+            }
+        }
+    }
+    else
+    {
+        printf("\nfirst create list then insert\n");
+    }
+}
+void delete_at_index()
+{
+    if (head != NULL)
+    {
+        int index;
+        printf("enter index : ");
+        scanf("%d", &index); // 0
+        if (index == 0)
+        {
+            delete_at_frist();
+        }
+        else
+        {
+            struct node *temp = head;
+            int c = 0;
+            while (temp != NULL)
+            {
+                c++;
+                temp = temp->next;
+            }
+
+            if (index == c - 1)
+            {
+                delete_at_last();
+            }
+            else if (index > c - 1 || index < 0)
+            {
+                printf("\ninvalid index\n");
+            }
+            else
+            {
+                struct node *prev, *temp;
+                int i = 0;
+                temp = head;
+                while (i < index)
+                {
+                    i++;
+                    prev = temp;
+                    temp = temp->next;
+                }
+                prev->next = temp->next;
+                printf("\n%d is deleted from list\n", temp->data);
+                free(temp);
+            }
+        }
+    }
+    else
+    {
+        printf("\nfirst create list then delete\n");
+    }
+}
+void search_element()
+{
+    if (head != NULL)
+    {
+        int num;
+        printf("enter a num : ");
+        scanf("%d", &num); // 60
+        struct node *temp = head;
+        while (temp != NULL)
+        {
+            if (temp->data == num)
+            {
+                printf("\nelement is found\n");
+                return;
+            }
+            temp = temp->next;
+        }
+        printf("\nelement is not found\n");
+    }
+    else
+    {
+
+        printf("\nfirst create list then search\n");
+    }
+}
+void main()
+{
+    int num;
+    while (1)
+    {
+        printf("\nWelcome to my circuler Linked List Program:\n");
+        printf("Press 1 to create list\n");
+        printf("Press 2 to display\n");
+        printf("Press 3 to insert element at the frist\n");
+        printf("Press 4 to delete element at the frist\n");
+        printf("Press 5 to insert element at last\n");
+        printf("Press 6 to delete element at the last\n");
+        printf("Press 7 to insert element at index\n");
+        printf("Press 8 to delete element at index\n");
+        printf("Press 9 to search element in list\n");
+        printf("Press 10 to exit\n");
+        printf("choose option : ");
+        scanf("%d", &num);
+        switch (num)
+        {
+        case 1:
+            create_list();
+            break;
+        case 2:
+            display_list();
+            break;
+        case 3:
+            insert_at_frist();
+            break;
+        case 4:
+            delete_at_frist();
+            break;
+        case 5:
+            insert_at_last();
+            break;
+        case 6:
+            delete_at_last();
+            break;
+        case 7:
+            insert_at_index();
+            break;
+        case 8:
+            delete_at_index();
+            break;
+        case 9:
+            search_element();
+            break;
+        case 10:
+            exit(0); // Exit the program
+        default:
+            printf("Entered wrong number\n");
+            break;
+        }
+    }
+}
