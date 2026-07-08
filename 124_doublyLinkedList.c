b04aaab60462ba5161918bfc0fffde042da2d7a7
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-07-08 19:12:11 +0530

    07/07

--- 124_doublyLinkedList.c
+++ 124_doublyLinkedList.c
@@ -46,14 +46,14 @@ void create_list()
     }
     else
     {
-        printf("your doubly linked list is already creted");
+        printf("\nyour doubly linked list is already creted\n");
     }
 }
 void display_list()
 {
     if (head != NULL)
     {
-        printf("doubly linked list element are  :  \n");
+        printf("\ndoubly linked list element are  :  \n");
         struct node *temp = head;
         while (temp != NULL)
         {
@@ -61,13 +61,182 @@ void display_list()
             temp = temp->next;
         }
         printf("\n");
+
+        struct node *tail;
+        temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        tail = temp;
+
+        printf("doubly linked list reverse element are  :  \n");
+        while (tail != NULL)
+        {
+            printf("%d ", tail->data);
+            tail = tail->prev;
+        }
+        printf("\n");
     }
     else
     {
-        printf("first create doubly linked list then display");
+        printf("\nfirst create doubly linked list then display\n");
     }
 }
 
+void insert_at_frist()
+{
+    if (head != NULL)
+    {
+        int num;
+        struct node *new;
+        printf("enter num : ");
+        scanf("%d", &num);
+        new = (struct node *)malloc(sizeof(struct node));
+        new->data = num;
+        new->next = head;
+        new->prev = NULL;
+        head->prev = new;
+        head = new;
+    }
+    else
+    {
+        printf("\nfirst create doubly linked list then insert\n");
+    }
+}
+void delete_at_frist()
+{
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            printf("\n%d element is deleted \n", head->data);
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            struct node *temp = head;
+            head->next->prev = NULL;
+            head = head->next;
+            printf("\n%d element is deleted \n", temp->data);
+            free(temp);
+        }
+    }
+    else
+    {
+        printf("\nfirst create doubly linked list then delete\n");
+    }
+}
+void insert_at_last()
+{
+    if (head != NULL)
+    {
+        int num;
+        struct node *new;
+        printf("enter num : ");
+        scanf("%d", &num);
+        new = (struct node *)malloc(sizeof(struct node));
+        new->data = num;
+        new->next = NULL;
+        new->prev = NULL;
+        struct node *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = new;
+        new->prev = temp;
+    }
+    else
+    {
+        printf("\nfirst create doubly linked list then insert\n");
+    }
+}
+void delete_at_last()
+{
+    if (head != NULL)
+    {
+        if (head->next == NULL)
+        {
+            printf("\n%d element is deleted \n", head->data);
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            struct node *temp = head;
+            while (temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->prev->next = NULL;
+            printf("\n%d element is deleted \n", temp->data);
+            free(temp);
+        }
+    }
+    else
+    {
+        printf("\nfirst create doubly linked list then delete\n");
+    }
+}
+void insert_at_index()
+{
+    if (head != NULL)
+    {
+        int index;
+        printf("enter a index : ");
+        scanf("%d", &index);
+        if (index == 0)
+        {
+            insert_at_frist();
+        }
+        else
+        {
+            struct node *temp = head;
+            int c = 0;
+            while (temp != NULL)
+            {
+                temp = temp->next;
+                c++;
+            }
+            if (c == index)
+            {
+                insert_at_last();
+            }
+            else if (index < 0 || index > c)
+            {
+                printf("\ninvalid index number");
+            }
+            else
+            {
+                temp = head;
+                int i = 0;
+                while (i < index)
+                {
+                    temp = temp->next;
+                    i++;
+                }
+
+                int num;
+                struct node *new;
+                printf("enter num : ");
+                scanf("%d", &num);
+                new = (struct node *)malloc(sizeof(struct node));
+                new->data = num;
+                new->prev = temp->prev;
+                new->next = temp;
+                temp->prev->next = new;
+                temp->prev = new;
+            }
+        }
+    }
+    else
+    {
+        printf("\nfirst create doubly linked list then insert\n");
+    }
+}
 void main()
 {
     int num;
@@ -83,7 +252,8 @@ void main()
         printf("Press 7 to insert element at index\n");
         printf("Press 8 to delete element at index\n");
         printf("Press 9 to search element in list\n");
-        printf("Press 10 to exit\n");
+        printf("Press 10 to reverse display\n");
+        printf("Press 11 to exit\n");
         printf("choose option : ");
         scanf("%d", &num);
         switch (num)
@@ -94,29 +264,30 @@ void main()
         case 2:
             display_list();
             break;
-        // case 3:
-        //     insert_at_frist();
-        //     break;
-        // case 4:
-        //     delete_at_frist();
-        //     break;
-        // case 5:
-        //     insert_at_last();
-        //     break;
-        // case 6:
-        //     delete_at_last();
-        //     break;
-        // case 7:
-        //     insert_at_index();
-        //     break;
+        case 3:
+            insert_at_frist();
+            break;
+        case 4:
+            delete_at_frist();
+            break;
+        case 5:
+            insert_at_last();
+            break;
+        case 6:
+            delete_at_last();
+            break;
+        case 7:
+            insert_at_index();
+            break;
         // case 8:
         //     delete_at_index();
         //     break;
         // case 9:
         //     search_element();
         //     break;
-        case 10:
+        case 11:
             exit(0);
+            break;
         default:
             printf("Entered wrong number\n");
             break;
